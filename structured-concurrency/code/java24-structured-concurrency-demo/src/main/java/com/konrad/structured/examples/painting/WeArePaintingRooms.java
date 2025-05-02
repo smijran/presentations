d5c674467f5d05b716ae398c08b9568723b5935c
Konrad <konrad.szalkowski@gmail.com>
2025-05-02 22:34:25 +0200

    Scoped values example moved aside

--- structured-concurrency/code/java24-structured-concurrency-demo/src/main/java/com/konrad/structured/examples/painting/WeArePaintingRooms.java
+++ structured-concurrency/code/java24-structured-concurrency-demo/src/main/java/com/konrad/structured/examples/painting/WeArePaintingRooms.java
@@ -1,4 +1,4 @@
-package com.konrad.structured.examples.party;
+package com.konrad.structured.examples.painting;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
