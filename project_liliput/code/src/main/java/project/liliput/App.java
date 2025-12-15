046b2420d6615bb34766257991236200458d859a
Konrad Szałkowski <konrad.szalkowski@gmail.com>
2025-12-15 09:20:14 +0100

    Code for liliput LT

--- project_liliput/code/src/main/java/project/liliput/App.java
+++ project_liliput/code/src/main/java/project/liliput/App.java
@@ -1,11 +1,25 @@
 package project.liliput;
 
 public class App {
-    public String getGreeting() {
-        return "Hello from Project Liliput!";
-    }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+	private final static class SomeObject {
+	}
+
+	private final static int NUM_OF_OBJECTS = 1000;
+
+	public static void main() {
+		int[] address = new int[NUM_OF_OBJECTS];
+		for (int i = 0; i < NUM_OF_OBJECTS; i++) {
+			address[i] = System.identityHashCode(new SomeObject());
+		}
+		for (int i = 0; i < NUM_OF_OBJECTS; i++) {
+			System.out.printf("Object %d at addr: 0x%08X\n", i, address[i]);
+		}
+        int size = (address[NUM_OF_OBJECTS - 1] - address[NUM_OF_OBJECTS - 2]);
+        System.out.println("Estimated size of object: " + size);
+        System.out.println("Estimated size of all object: " + size * NUM_OF_OBJECTS);
+
+	}
+  
+
 }
